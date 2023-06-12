22c4dba13e3fd4102b2d643f630211c0a64bf864
Valentynmrzv <bmvalentin27@gmail.com>
2023-06-12 10:47:28 +0300

    btn js

--- js/button.js
+++ js/button.js
@@ -1,32 +1,65 @@
 
-document.querySelector(".button__btn.all-btn").addEventListener('click', function () {
-    document.querySelectorAll(".samples__item.site").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.application").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.design").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.marketing").classList.remove('hidden');
-    
+// Обработчик события для кнопки .button__btn.all-btn
+document.querySelector('.button__btn.all-btn').addEventListener('click', function() {
+  var items = document.querySelectorAll('.samples__item.site, .samples__item.application, .samples__item.design, .samples__item.marketing');
+  
+  for (var i = 0; i < items.length; i++) {
+    items[i].classList.remove('hidden');
+  }
 });
-document.querySelector(".button__btn.site-btn").addEventListener('click', function () {
-    document.querySelectorAll(".samples__item.site").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.application").classList.add('hidden');
-    document.querySelectorAll(".samples__item.design").classList.add('hidden');
-    document.querySelectorAll(".samples__item.marketing").classList.add('hidden');
+
+// Обработчик события для кнопки .button__btn.site-btn
+document.querySelector('.button__btn.site-btn').addEventListener('click', function() {
+  var siteItems = document.querySelectorAll('.samples__item.site');
+  var otherItems = document.querySelectorAll('.samples__item.application, .samples__item.design, .samples__item.marketing');
+  
+  for (var i = 0; i < siteItems.length; i++) {
+    siteItems[i].classList.remove('hidden');
+  }
+  
+  for (var j = 0; j < otherItems.length; j++) {
+    otherItems[j].classList.add('hidden');
+  }
 });
-document.querySelector(".button__btn.application-btn").addEventListener('click', function () {
-    document.querySelectorAll(".samples__item.application").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.site").classList.add('hidden');
-    document.querySelectorAll(".samples__item.design").classList.add('hidden');
-    document.querySelectorAll(".samples__item.marketing").classList.add('hidden');
+
+// Обработчик события для кнопки .button__btn.application-btn
+document.querySelector('.button__btn.application-btn').addEventListener('click', function() {
+  var applicationItems = document.querySelectorAll('.samples__item.application');
+  var otherItems = document.querySelectorAll('.samples__item.site, .samples__item.design, .samples__item.marketing');
+  
+  for (var i = 0; i < applicationItems.length; i++) {
+    applicationItems[i].classList.remove('hidden');
+  }
+  
+  for (var j = 0; j < otherItems.length; j++) {
+    otherItems[j].classList.add('hidden');
+  }
 });
-document.querySelector(".button__btn.design-btn").addEventListener('click', function () {
-    document.querySelectorAll(".samples__item.design").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.site").classList.add('hidden');
-    document.querySelectorAll(".samples__item.application").classList.add('hidden');
-    document.querySelectorAll(".samples__item.marketing").classList.add('hidden');
+
+// Обработчик события для кнопки .button__btn.design-btn
+document.querySelector('.button__btn.design-btn').addEventListener('click', function() {
+  var designItems = document.querySelectorAll('.samples__item.design');
+  var otherItems = document.querySelectorAll('.samples__item.site, .samples__item.application, .samples__item.marketing');
+  
+  for (var i = 0; i < designItems.length; i++) {
+    designItems[i].classList.remove('hidden');
+  }
+  
+  for (var j = 0; j < otherItems.length; j++) {
+    otherItems[j].classList.add('hidden');
+  }
 });
-document.querySelector(".button__btn.marketing-btn").addEventListener('click', function () {
-    document.querySelectorAll(".samples__item.marketing").classList.remove('hidden');
-    document.querySelectorAll(".samples__item.site").classList.add('hidden');
-    document.querySelectorAll(".samples__item.application").classList.add('hidden');
-    document.querySelectorAll(".samples__item.design").classList.add('hidden');
+
+// Обработчик события для кнопки .button__btn.marketing-btn
+document.querySelector('.button__btn.marketing-btn').addEventListener('click', function() {
+  var marketingItems = document.querySelectorAll('.samples__item.marketing');
+  var otherItems = document.querySelectorAll('.samples__item.site, .samples__item.application, .samples__item.design');
+  
+  for (var i = 0; i < marketingItems.length; i++) {
+    marketingItems[i].classList.remove('hidden');
+  }
+  
+  for (var j = 0; j < otherItems.length; j++) {
+    otherItems[j].classList.add('hidden');
+  }
 });
